7c46308d37dccdc272633575ec8e2b7f7df0a02a
luka <lukachikashvili2020@gmail.com>
2024-02-24 20:41:34 +0400

    add responsive design to hero section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div className='text-white  w-full flex items-center justify-between py-8 px-16 text-2xl'>
+    <div className=' text-white  w-full flex items-center justify-between py-8 px-16 text-2xl '>
       <div className="logo">
           <h2 className='text-4xl'><span className='text-[#C70039]'>Artify</span>Gallery</h2>
       </div>
 
-   <nav className='flex gap-8'>
+   <nav className='hidden md:flex gap-8 '>
           <Link to = "/">Home</Link>
           <Link to = "/">Artists</Link>
           <Link to = "/">ArtWork</Link>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,14 +13,14 @@ const Home = () => {
          
         
           
-           <div className='flex items-center justify-center gap-8 '>
-           <img src = {art4} className='w-1/4 mt-12 shadow-lg shadow-black' />
-           <img src = {art2} className='w-1/4 mb-36 shadow-lg shadow-black' />
-           <img src = {art1}  className='w-1/4 shadow-lg shadow-black' />
-           <img src = {art3} className='w-1/4 mb-36 shadow-lg shadow-black' />
+           <div className='hidden md:flex items-center justify-center gap-8  '>
+           <img src = {art4} className='w-1/4 mt-12 shadow-lg shadow-black duration-500 ease hover:opacity-50' />
+           <img src = {art2} className='w-1/4 mb-36 shadow-lg shadow-black duration-500 ease hover:opacity-50'  />
+           <img src = {art1}  className='w-1/4 shadow-lg shadow-black duration-500 ease hover:opacity-50' />
+           <img src = {art3} className='w-1/4 mb-36 shadow-lg shadow-black duration-500 ease hover:opacity-50' />
            </div>
 
-           <h1 className='text-white text-4xl w-3/5 text-center leading-10'>Discover, Connect, Collect - <span className='text-[#C70039]'>ArtifyGallery,</span> Where Every Brushstroke Tells a Story.</h1>
+           <h1 className='text-white text-2xl w-full md:text-4xl md:w-3/5 text-center leading-10'>Discover, Connect, Collect - <span className='text-[#C70039]'>ArtifyGallery,</span> Where Every Brushstroke Tells a Story.</h1>
            <button className='text-white bg-[#C70039] py-2 px-12 text-2xl rounded-md shadow-lg border-2 border-transparent shadow-black mt-12 duration-500 ease-in hover:bg-transparent border-[#C70039]'>Discover Artists</button>
       </div>
     </div>
