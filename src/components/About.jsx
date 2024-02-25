edbc6fdad788296091648b3b06b8a19783de697e
luka <lukachikashvili2020@gmail.com>
2024-02-25 15:02:46 +0400

    designed about us section

---
+++ src/components/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='min-h-screen '>
+
+      <div className='flex items-center'>
+          <h2 className='text-white text-5xl p-12 line'>About Us</h2>
+      </div>
+
+      <div className='w-4/5 m-auto flex flex-col gap-8 pt-16 '>
+          <p className='text-white text-4xl '>At ArtifyGallery, we are passionate about connecting art enthusiasts with talented 
+               emerging artists from around the world. Our mission
+                is to provide a platform where artists can showcase their unique visions 
+               and creations while offering art lovers the opportunity to discover and collect 
+               exceptional artwork that resonates with them.</p>
+<p className='text-white text-4xl'>Our team at ArtifyGallery is comprised of passionate art lovers and industry professionals who are committed to providing a seamless and enriching experience for both artists and collectors. We strive to uphold the highest standards of integrity, authenticity, and excellence in everything we do, ensuring that every interaction with ArtifyGallery is memorable and rewarding.</p>
+      </div>
+    </div>
+  )
+}
+
+export default About
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,13 +3,15 @@ import art1 from '../assets/art.jpg';
 import art2 from '../assets/art2.jpg';
 import art3 from '../assets/art3.jpg';
 import art4 from '../assets/art4.jpg';
+import About from './About';
 
 
 
 const Home = () => {
   return (
-    <div className= 'flex items-center justify-center h-screen home' >
-      <div className='flex flex-col items-center justify-between  px-36 pb-36'>
+    <>
+    <div className= 'flex items-center justify-center  home' >
+      <div className='flex flex-col items-center justify-between h-screen  px-36 pb-36'>
          
         
           
@@ -23,7 +25,12 @@ const Home = () => {
            <h1 className='text-white text-2xl w-full md:text-4xl md:w-3/5 text-center leading-10'>Discover, Connect, Collect - <span className='text-[#C70039]'>ArtifyGallery,</span> Where Every Brushstroke Tells a Story.</h1>
            <button className='text-white bg-[#C70039] py-2 px-12 text-2xl rounded-md shadow-lg border-2 border-transparent shadow-black mt-12 duration-500 ease-in hover:bg-transparent border-[#C70039]'>Discover Artists</button>
       </div>
+
+ 
     </div>
+
+<About />
+</>
   )
 }
 
