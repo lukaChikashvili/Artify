b3983bdb101f21bf42ed7301b9cab755f6f6cfed
luka <lukachikashvili2020@gmail.com>
2024-02-29 14:01:38 +0400

    added framer motion animation to AllArtist section

--- src/components/AllArtists.jsx
+++ src/components/AllArtists.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Data } from './Data';
 import ArtistComp from './ArtistComp';
+import {motion} from 'framer-motion';
+
 
 const AllArtists = () => {
   return (
     <div>
-     <h2 className='text-white text-5xl p-12 line'>All artists</h2>
+     <motion.h2 initial = {{opacity: 0, translateX: -10}} whileInView = {{opacity: 1, translateX: 0}} transition = {{duration: 1, delay: 1}} className='text-white text-5xl p-12 line'>All artists</motion.h2>
 
    <div className='flex flex-wrap items-center justify-center gap-16'>
-   {Data.map((artist) => {
-    return <ArtistComp name = {artist.name} img = {artist.img} date = {artist.date}/>
+   {Data.map((artist, i) => {
+    return <ArtistComp name = {artist.name} img = {artist.img} date = {artist.date} index = {i} />
    })}
      
    </div>
--- src/components/ArtistComp.jsx
+++ src/components/ArtistComp.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import {motion} from 'framer-motion';
 
-const ArtistComp = ({name, img, date}) => {
+
+const ArtistComp = ({name, img, date, index}) => {
   return (
-     <div className='flex flex-col gap-4 py-12'>
+     <motion.div initial = {{opacity: 0, translateX: -10}} whileInView = {{opacity: 1, translateX: 0}} transition = {{duration: 0.4, delay: index * 0.25}}  className='flex flex-col gap-4 py-12'>
      <img src = {img} className='w-72 h-72 object-cover shadow-lg shadow-black duration-500 ease-in hover:opacity-50' />
      <h2 className='text-white text-3xl text-center'>{name}</h2>
      <span className='text-center text-[#C70039] text-4xl'>{date}</span>
-     </div>
+     </motion.div>
   )
 }
 
