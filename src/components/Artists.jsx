a744348257220ca06edaa278b104225dfcd032a1
luka <lukachikashvili2020@gmail.com>
2024-02-29 13:46:07 +0400

    wrote artists in array and then maped it to add framer motion delay

--- src/components/Artists.jsx
+++ src/components/Artists.jsx
@@ -8,40 +8,59 @@ import {useNavigate} from 'react-router-dom';
 
 
 const Artists = () => {
+  const artist = [
+    {
+      id: 1,
+      img: leo,
+      name: "leonardo da vinci",
+      date: "1452 - 1519"
+ },
+
+ {
+      id: 2,
+      img: gogh,
+      name: "Vincent van gogh",
+      date: "1853 - 1890"
+ },
+
+ {
+      id: 3,
+      img: pablo,
+      name: "Pablo Picasso",
+      date: "1881 - 1973"
+ },
+
+ {
+      id: 4,
+      img: rembr,
+      name: "Rembrandt",
+      date: "1606 - 1669"
+ }
+
+  ];
+
 
   let navigate = useNavigate();
   
   return (
     <div className='min-h-screen py-12'>
       <h2 className='text-white text-5xl p-12 line'>Trending Artists</h2>
-
       <div className='flex flex-col md:flex-row items-center justify-start gap-8 px-12 py-8'>
-          <div className='flex flex-col gap-4'>
-          <img src = {leo} className='w-72 h-72 object-cover shadow-lg shadow-black duration-500 ease-in hover:opacity-50' />
-          <h2 className='text-white text-3xl text-center'>Leonardo da vinci</h2>
-          <span className='text-center text-[#C70039] text-4xl'>1452 - 1519</span>
-          </div>
+       {artist.map((value, i) => (
 
+        
           <div className='flex flex-col gap-4'>
-          <img src = {gogh} className='w-72 h-72 object-cover shadow-lg shadow-black duration-500 ease-in hover:opacity-50'/>
-          <h2 className='text-white text-3xl text-center'>Vincent van gogh</h2>
-          <span className='text-center text-[#C70039] text-4xl'>1853 - 1890</span>
-          </div>
-          
-          <div className='flex flex-col gap-4'>
-          <img src = {rembr} className='w-72 h-72 object-cover shadow-lg shadow-black duration-500 ease-in hover:opacity-50'/>
-          <h2 className='text-white text-3xl text-center'>Rembrandt</h2>
-          <span className='text-center text-[#C70039] text-4xl'>1606 - 1669</span>
-          </div>
+          <img src = {value.img} className='w-72 h-72 object-cover shadow-lg shadow-black duration-500 ease-in hover:opacity-50' />
+          <h2 className='text-white text-3xl text-center'>{value.name}</h2>
+          <span className='text-center text-[#C70039] text-4xl'>{value.date}</span>
+ </div>
 
-          <div className='flex flex-col gap-4'>
-          <img src = {pablo} className='w-72 h-72 object-cover shadow-lg shadow-black duration-500 ease-in hover:opacity-50'/>
-          <h2 className='text-white text-3xl text-center'>Pablo Picasso</h2>
-          <span className='text-center text-[#C70039] text-4xl'>1881 - 1973</span>
-          </div>
-          
-      </div>
 
+
+
+       ))}
+      
+</div>
       <button className='bg-transparent text-white border-2 border-[#C70039] py-4 px-12 text-2xl ml-12 cursor-pointer rounded-md mt-8 duration-1000 ease hover:bg-[#C70039]' onClick={() => navigate('/artists')}>Discover all artists</button>
     </div>
   )
