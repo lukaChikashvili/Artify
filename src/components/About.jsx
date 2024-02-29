9e382300f6ea6e6ce7fca4b090193c32bf2b97cd
luka <lukachikashvili2020@gmail.com>
2024-02-29 13:41:16 +0400

    added framer motion on about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
+import {motion} from 'framer-motion';
+
 
 const About = () => {
   return (
     <div className='min-h-screen '>
 
       <div >
-          <h2 className='text-white text-5xl p-12 line'>About Us</h2>
+          <motion.h2 initial = {{opacity: 0, translateX: -10}} whileInView = {{opacity: 1, translateX: 0}} transition = {{duration: 0.4, delay: 0.5}} className='text-white text-5xl p-12 line'>About Us</motion.h2>
       </div>
 
       <div className='w-4/5 m-auto flex flex-col gap-8 pt-16 '>
-          <p className='text-white text-4xl '>At ArtifyGallery, we are passionate about connecting art enthusiasts with talented 
+          <motion.p initial = {{opacity: 0, translateX: -10}} whileInView = {{opacity: 1, translateX: 0}} transition = {{duration: 1, delay: 0.5}} className='text-white text-4xl '>At ArtifyGallery, we are passionate about connecting art enthusiasts with talented 
                emerging artists from around the world. Our mission
                 is to provide a platform where artists can showcase their unique visions 
                and creations while offering art lovers the opportunity to discover and collect 
-               exceptional artwork that resonates with them.</p>
-<p className='text-white text-4xl'>Our team at ArtifyGallery is comprised of passionate art lovers and industry professionals who are committed to providing a seamless and enriching experience for both artists and collectors. We strive to uphold the highest standards of integrity, authenticity, and excellence in everything we do, ensuring that every interaction with ArtifyGallery is memorable and rewarding.</p>
+               exceptional artwork that resonates with them.</motion.p>
+<motion.p initial = {{opacity: 0, translateX: -10}} whileInView = {{opacity: 1, translateX: 0}} transition = {{duration: 1, delay: 0.8}}  className='text-white text-4xl'>Our team at ArtifyGallery is comprised of passionate art lovers and industry professionals who are committed to providing a seamless and enriching experience for both artists and collectors. We strive to uphold the highest standards of integrity, authenticity, and excellence in everything we do, ensuring that every interaction with ArtifyGallery is memorable and rewarding.</motion.p>
       </div>
     </div>
   )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,6 +6,8 @@ import art4 from '../assets/art4.jpg';
 import About from './About';
 import Artists from './Artists';
 import Contact from './Contact';
+import {motion} from 'framer-motion';
+
 
 
 
@@ -19,14 +21,14 @@ const Home = () => {
         
           
            <div className='hidden md:flex items-center justify-center gap-8  '>
-           <img src = {art4} className='w-1/4 mt-12 shadow-lg shadow-black duration-500 ease hover:opacity-50' />
-           <img src = {art2} className='w-1/4 mb-36 shadow-lg shadow-black duration-500 ease hover:opacity-50'  />
-           <img src = {art1}  className='w-1/4 shadow-lg shadow-black duration-500 ease hover:opacity-50' />
-           <img src = {art3} className='w-1/4 mb-36 shadow-lg shadow-black duration-500 ease hover:opacity-50' />
+           <motion.img initial = {{opacity: 0, translateX: -10}} animate = {{opacity: 1, translateX: 0}} transition = {{duration: 1, delay: 0.5}} src = {art4} className='w-1/4 mt-12 shadow-lg shadow-black duration-500 ease hover:opacity-50' />
+           <motion.img initial = {{opacity: 0, translateX: -10}} animate = {{opacity: 1, translateX: 0}} transition = {{duration: 1, delay: 1}} src = {art2} className='w-1/4 mb-36 shadow-lg shadow-black duration-500 ease hover:opacity-50'  />
+           <motion.img initial = {{opacity: 0, translateX: -10}} animate = {{opacity: 1, translateX: 0}} transition = {{duration: 1, delay: 1.3}} src = {art1}  className='w-1/4 shadow-lg shadow-black duration-500 ease hover:opacity-50' />
+           <motion.img initial = {{opacity: 0, translateX: -10}} animate = {{opacity: 1, translateX: 0}} transition = {{duration: 1, delay: 1.7}} src = {art3} className='w-1/4 mb-36 shadow-lg shadow-black duration-500 ease hover:opacity-50' />
            </div>
 
-           <h1 className='text-white text-2xl w-full md:text-4xl md:w-3/5 text-center leading-10'>Discover, Connect, Collect - <span className='text-[#C70039]'>ArtifyGallery,</span> Where Every Brushstroke Tells a Story.</h1>
-           <button className='text-white bg-[#C70039] py-2 px-12 text-2xl rounded-md shadow-lg border-2 border-transparent shadow-black mt-12 duration-500 ease-in hover:bg-transparent border-[#C70039]'>Discover Artists</button>
+           <motion.h1 initial = {{opacity: 0}} animate = {{opacity: 1}} transition = {{duration: 1.5, delay: 1.5}}  className='text-white text-2xl w-full md:text-4xl md:w-3/5 text-center leading-10'>Discover, Connect, Collect - <span className='text-[#C70039]'>ArtifyGallery,</span> Where Every Brushstroke Tells a Story.</motion.h1>
+           <motion.button initial = {{opacity: 0}} animate = {{opacity: 1}} transition = {{duration: 1, delay: 2}} className='text-white bg-[#C70039] py-2 px-12 text-2xl rounded-md shadow-lg border-2 border-transparent shadow-black mt-12 duration-500 ease-in hover:bg-transparent border-[#C70039]'>Discover Artists</motion.button>
       </div>
 
  
